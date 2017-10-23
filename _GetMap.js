64faf942f5ed18b0743f160e7b40833ed51cfb8b
bamboohiko <bamboohiko@163.com>
2017-10-23 13:45:01 +0800

    20171023 1344

--- _GetMap.js
+++ _GetMap.js
@@ -5,9 +5,11 @@ function GetMap()
         'AonZHtr87029fCW4keG_qAIVE_76xXDXPMPyB8a79QHPzM57N9wuRGXVeced35ev',
         center: new Microsoft.Maps.Location(13.495191631943101, 100.41977246667943),
         mapTypeId: Microsoft.Maps.MapTypeId.aerial,
+        zoom: 10
         
     });
 
+    var locs = [];
 
     for (var i = 0;i < data.features.length; i++) {
         if (data.features[i].geometry != null) {
@@ -18,83 +20,34 @@ function GetMap()
                 exteriorRing.push(new Microsoft.Maps.Location(cd[j][1],cd[j][0]));
             }
 
+            locs = locs.concat(exteriorRing);
+
             //Create a polygon
             var polygon = new Microsoft.Maps.Polygon(exteriorRing, {
-                fillColor: 'rgba(0, 255, 0, 0.5)',
+                //fillColor: 'rgba(0, 255, 0, 0.5)',
                 strokeColor: 'red',
                 strokeThickness: 2
             });
 
             //Add the polygon to map
-            map.entities.push(polygon);            
+            map.entities.push(polygon);
+
+            /*
+            Microsoft.Maps.Events.addHandler(polygon, 'click', function(e) {
+                document.getElementById('output').innerHTML += ',' + i;
+
+                setTimeout(function () { document.getElementById('output').innerHTML = null; }, 1000);
+            });
+            */  
         }
     }
-    /*
-    //Add your post map load code here.
-    $.ajax({
-        type: "GET",
-        url: "th2.json",
-        dataType: "json",
-        success: function (data) {
-            for (var i = 0; i < data.length; i++) {
-                if (item.geometry != null) {
-                    var exteriorRing = item.geometry;
 
-                    //Create a polygon
-                    var polygon = new Microsoft.Maps.Polygon(exteriorRing, {
-                        fillColor: 'rgba(0, 255, 0, 0.5)',
-                        strokeColor: 'red',
-                        strokeThickness: 2
-                    });
+    var rect = Microsoft.Maps.LocationRect.fromLocations(locs);
 
-                    //Add the polygon to map
-                    map.entities.push(polygon);
-                }
-            }
-        }
+    map.setView({ 
+        bounds: rect,
+        padding: 80 
+
     });
-    */
-
-    /*
-    $.getJSON('th2.json',function(data) {
-        $.each(data.features, function(i, item) {
-            if (item.geometry != null) {
-                var exteriorRing = item.geometry;
-
-                //Create a polygon
-                var polygon = new Microsoft.Maps.Polygon(exteriorRing, {
-                    fillColor: 'rgba(0, 255, 0, 0.5)',
-                    strokeColor: 'red',
-                    strokeThickness: 2
-                });
-
-                //Add the polygon to map
-                map.entities.push(polygon);
-            }
-        })
-    })
-    */
-
-
-    /*
-    var center = map.getCenter();
     
-    //Create array of locations to form a ring.
-    var exteriorRing = [
-        center,
-        new Microsoft.Maps.Location(center.latitude - 0.5, center.longitude - 1),
-        new Microsoft.Maps.Location(center.latitude - 0.5, center.longitude + 1),
-        center
-    ];
-
-    //Create a polygon
-    var polygon = new Microsoft.Maps.Polygon(exteriorRing, {
-        fillColor: 'rgba(0, 255, 0, 0.5)',
-        strokeColor: 'red',
-        strokeThickness: 2
-    });
-
-    //Add the polygon to map
-    map.entities.push(polygon);
-    */
 }
